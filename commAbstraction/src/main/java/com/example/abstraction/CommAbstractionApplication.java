31639f50f729fb0e737cad022d4f025de34792c4
bm12m <bm12m@192.168.1.145>
2018-02-04 20:27:15 -0500

    first commit

---
+++ commAbstraction/src/main/java/com/example/abstraction/CommAbstractionApplication.java
@@ -0,0 +1,12 @@
+package com.example.abstraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommAbstractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommAbstractionApplication.class, args);
+	}
+}
